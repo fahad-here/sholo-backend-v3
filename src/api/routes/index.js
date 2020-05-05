0c58f6a4e12e30e5deb58bf6fd3605a0e6a4c3ce
fahad_laptop <fmohajir@gmail.com>
2020-05-05 16:05:01 +0400

    added prettier and dotenv

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-    res.json({error: false})
+    res.json({ error: false })
 })
 
 module.exports = router
