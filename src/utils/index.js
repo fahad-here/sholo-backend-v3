75b5bdee7b4ca689eec039f8c20e293346d69160
fahad_laptop <fmohajir@gmail.com>
2020-05-07 17:42:54 +0400

    added GetCandleKey util function

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,9 +2,12 @@ const Logger = require('./logger')
 const ResponseMessage = require('./getResponseMessage')
 const RouteErrorHandler = require('./errorHandler')
 const Validation = require('./validation')
+const GetCandleKey = (exchangeName, symbol, timeFrame) =>
+    `${exchangeName}__${symbol}__${timeFrame}`
 module.exports = {
     Logger,
     ResponseMessage,
     RouteErrorHandler,
-    Validation
+    Validation,
+    GetCandleKey
 }
