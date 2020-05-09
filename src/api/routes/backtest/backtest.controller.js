6e1897d262d15e6ab5a82160764bdbf61a230474
fahad_laptop <fmohajir@gmail.com>
2020-05-10 01:18:34 +0400

    added backtest middleware to the routes

--- src/api/routes/backtest/backtest.controller.js
+++ src/api/routes/backtest/backtest.controller.js
@@ -1,16 +1,58 @@
 const express = require('express')
 const backTestController = express.Router()
-const { AuthMiddleware } = require('../../middleware')
+const { AuthMiddleware, BackTestMiddleware } = require('../../middleware')
 const { Validation } = require('../../../utils')
 const { ValidateBody, Schemas } = Validation
+const { AddBackTestConfig } = Schemas
 
-backTestController.get('/', AuthMiddleware.requireJWT)
-backTestController.post('/', AuthMiddleware.requireJWT)
-backTestController.put('/:id', AuthMiddleware.requireJWT)
-backTestController.delete('/:id', AuthMiddleware.requireJWT)
-backTestController.post('/:id/run', AuthMiddleware.requireJWT)
-backTestController.get('/:id', AuthMiddleware.requireJWT)
-backTestController.get('/results', AuthMiddleware.requireJWT)
-backTestController.get('/results/:id', AuthMiddleware.requireJWT)
+backTestController.get(
+    '/',
+    AuthMiddleware.requireJWT,
+    BackTestMiddleware.getAllBackTestConfigs
+)
+
+backTestController.post(
+    '/',
+    AuthMiddleware.requireJWT,
+    ValidateBody(AddBackTestConfig),
+    BackTestMiddleware.addNewBackTest
+)
+
+backTestController.put(
+    '/:id',
+    AuthMiddleware.requireJWT,
+    ValidateBody(AddBackTestConfig),
+    BackTestMiddleware.editBackTest
+)
+
+backTestController.delete(
+    '/:id',
+    AuthMiddleware.requireJWT,
+    BackTestMiddleware.deleteBackTestConfig
+)
+
+backTestController.post(
+    '/:id/run',
+    AuthMiddleware.requireJWT,
+    BackTestMiddleware.runBackTestConfig
+)
+
+backTestController.get(
+    '/:id',
+    AuthMiddleware.requireJWT,
+    BackTestMiddleware.getBackTestConfig
+)
+
+backTestController.get(
+    '/results',
+    AuthMiddleware.requireJWT,
+    BackTestMiddleware.getBackTestResult
+)
+
+backTestController.get(
+    '/results/:id',
+    AuthMiddleware.requireJWT,
+    BackTestMiddleware.getAllBackTestResults
+)
 
 module.exports = backTestController
