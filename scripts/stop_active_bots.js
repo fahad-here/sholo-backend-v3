46d81836fb2462e5dd1db4b124cf55147b3ff028
fahad_laptop <fmohajir@gmail.com>
2020-08-12 21:52:54 +0400

    added force stop active bots script

---
+++ scripts/stop_active_bots.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+const { DBConnect } = require('../src/api/db')
+const {DBSchemas} = require('../src/api/db')
+const {
+    BotSchema,
+} = DBSchemas
+
+const _pauseBot = async (bot) => {
+    try {
+        let { active } = bot
+        if (!active)
+            return false
+        const bot = await BotSchema.findOneAndUpdate(
+            {
+                _id: bot._id,
+            },
+            {
+                $set: {
+                    active: false
+                }
+            },
+            { new: true }
+        )
+        return true
+    } catch (e) {
+        console.log(e)
+        return false
+    }
+}
+
+const _pauseAllRunningBots = async () => {
+    const allBots = await BotSchema.find({active: true})
+    console.log('Active Bots', allBots.length)
+    let results = {}
+    await allBots.map(async bot => {
+        const res = await _pauseBot(bot)
+        results[bot._id.toString()] = res
+    })
+    const checkConfigs = await BotSchema.find({active: true})
+    console.log('Check Bots', checkConfigs.length)
+    console.log('Results', results)
+}
+
+const pauseAllBots = async () =>{
+    await DBConnect()
+    await _pauseAllRunningBots()
+    process.exit()
+}
+
+pauseAllBots()
